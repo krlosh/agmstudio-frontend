592d982e40fc0a9240c4110d860760d5b698fd7c
krlosh <krlosh@gmail.com>
2017-04-01 12:42:36 +0200

    Primera tarea gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 
-gulp.task('html',function(){
+gulp.task('html',/*[tareas dependientes]*/function(){
   //Codigo de la tarea
+  gulp.src('src/html/**/*.html')
+    .pipe(gulp.dest('dist/'));
 });
