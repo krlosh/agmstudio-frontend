bedfad8645ab04f7100cd646f14b6934d71c0e39
krlosh <krlosh@gmail.com>
2017-04-01 20:01:05 +0200

    Definiendo estructura del proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,51 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var del = require('del');
+var path = require('path');
+
+var runSequence = require('run-sequence');
+var config ={
+  folders :{
+    dist : 'dist',
+    assets:'assets'
+  }
+};
+
+var paths ={
+  dist: path.join(config.folders.dist),
+  assets:path.join(config.folders.dist,
+      config.folders.assets),
+  html:path.join(config.folders.dist),
+  js:path.join(config.folders.dist,
+      config.folders.assets,'js'),
+  fonts:path.join(config.folders.dist,
+      config.folders.assets,'fonts'),
+  css:path.join(config.folders.dist,
+      config.folders.assets,'css'),
+  img:path.join(config.folders.dist,
+      config.folders.assets,'img'),
+};
 
 gulp.task('html',/*[tareas dependientes]*/function(){
   //Codigo de la tarea
   gulp.src('src/html/**/*.html')
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('js', function(){
+  gulp.src('src/js/**/*.js')
+      .pipe(concat('app.min.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest(paths.js));
+});
+
+gulp.task('default',function(){
+  runSequence('clean',
+              ['html','js']
+            );
+});
+
+gulp.task('clean',function(){
+  return del([paths.html]);
 });
